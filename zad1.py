3c9ebc373851cbecd8af8a9e658eaa1fcfba0b6b
alicja832 <alicja.zosia.k@gmail.com>
2023-10-23 09:48:05 +0200

    prawie poprawny

--- zad1.py
+++ zad1.py
@@ -4,72 +4,52 @@ import matplotlib.pyplot as plt
 
 t=["0.01","0.10","1."]
 zad=["","1.2:","1.3:"]
-zera=["","0","0"]
-files=[]
-colors=["bo","ro","go"]
+zera=["","","00"]
+files=[[],[],[]]
+colors=["b","r","g"]
+kl=0
 for i in zad:
-    kl=0
-    for j in t:
-        files.append("wynik"+i+j+zera[kl]+"0000.txt")
-        kl+=1
-for i in files:
+    for j in range(len(t)):
+        files[kl].append("wynik"+i+t[j]+zera[j]+"0000.txt")
+    kl+=1
 
-    data = np.loadtxt(i)
+for kl in range(3):
+    fig=plt.figure(facecolor='white')
+    
+    for i in range (len(files[kl])):
 
-    x = data[:, 0]
-    y_an = data[:, 2]
-    plt.plot(x, y_an, 'k-', lw=2, label='analityczne')
+        data = np.loadtxt(files[kl][i])
+       
+        x = data[:, 0]
+        if(i==0):
+            y_an = data[:, 2]
+            plt.plot(x, y_an, 'k-', lw=2, label='analityczne')
 
-    for j in range(3):
+        
         y = data[:, 1]
-        plt.plot(x, y, colors[j], label='dt='+t[j])
+        plt.plot(x, y, colors[i]+"o", label='dt='+t[i])
 
-        file = "wynik0.100000.txt"
-        data = np.loadtxt(file)
+
+    l1 = plt.legend()
+
+    plt.xlabel('t')
+    plt.ylabel('y(t)')
+
+    plt.savefig("zad"+str(kl+1)+".png",bbox_inches='tight', transparent=True)
+
+    fig=plt.figure()
+    for i in range(len(files[kl])):
+
+        data = np.loadtxt(files[kl][i])
 
         x = data[:, 0]
-        y = data[:, 1]
-        y_an = data[:, 2]
+        y = data[:, 3]
 
-        plt.plot(x, y, 'ro', label='dt=0.1')
+        plt.plot(x, y, colors[i]+"-", label='dt='+t[i])
 
-        file = "wynik1.000000.txt"
-        data = np.loadtxt(file)
+    plt.xlabel('t')
+    plt.ylabel('ynum(t)-ydok(t)')
 
-        x = data[:, 0]
-        y = data[:, 1]
-        y_an = data[:, 2]
+    l1 = plt.legend()
+    plt.savefig("zad"+str(kl+1)+"a.png",bbox_inches='tight', transparent=True)
 
-        plt.plot(x, y, 'go', label='dt=1')
-
-
-l1 = plt.legend()
-
-plt.xlabel('t')
-plt.ylabel('y(t)')
-
-plt.savefig("zad1.png",bbox_inches='tight', transparent=True)
-
-fig = plt.figure()
-x = data[:, 0]
-y_num=data[:,3]
-plt.plot(x, y_num, 'g-', lw=2, label='dt=1')
-
-file = "wynik0.100000.txt"
-data = np.loadtxt(file)
-x = data[:, 0]
-y_num=data[:,3]
-plt.plot(x, y_num, 'r-', lw=2, label='dt=0.1')
-
-file = "wynik0.010000.txt"
-data = np.loadtxt(file)
-x = data[:, 0]
-y_num=data[:,3]
-
-plt.plot(x, y_num, 'b-', lw=2, label='dt=0.01')
-plt.xlabel('t')
-plt.ylabel('ynum(t)-ydok(t)')
-
-l1 = plt.legend()
-plt.savefig("zad1a.png",bbox_inches='tight', transparent=True)
-#plt.show()
