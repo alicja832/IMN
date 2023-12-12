0d30fc9fd37dfbe0eed84dcae2297dc256658f9f
Alicja Kałuża <92887428+alicja832@users.noreply.github.com>
2023-12-12 08:10:47 +0100

    Add files via upload
    No prawie...

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,7 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-#define IT_MAX 2000
+#define IT_MAX 20000
 
 //parametry potrzebne do obliczen
 double delta=0.01;
@@ -34,10 +34,10 @@ void pierwsze_wb(std::vector<std::vector<double>>& v,double q_we)
     for(j=j1;j<=ny;j++)
     {
         y=j*delta;
-        v[0][j]=q_we/(2*ni)*(y*y*y/3.-y*y/2.*(j1*delta+ny*delta)+y*(j1*delta*ny*delta));
+        v[0][j]=q_we/(2*ni)*((y*y*y)/3.-(y*y)/2.*(j1*delta+ny*delta)+y*(j1*delta*ny*delta));
     }
     //brzeg C
-     for(j=j1;j<=ny;j++)
+     for(j=0;j<=ny;j++)
     {
         y=j*delta;
         v[nx][j]=q_we/(2*ni)*(y*y*y/3.-y*y/2.*(ny*delta))+(q_we*delta*j1*delta*j1*(-j1*delta+3*ny*delta))/(12*ni);
@@ -54,12 +54,12 @@ void pierwsze_wb(std::vector<std::vector<double>>& v,double q_we)
         v[i][0]=v[0][j1];
     }
     //brzeg E
-    for(j=1;i<j1;i++)
+    for(j=1;i<=j1;i++)
     {
         v[i1][i]=v[0][j1];
     }
     //brzeg F
-    for(i=1;i<i1;i++)
+    for(i=1;i<=i1;i++)
     {
         v[i][j1]=v[0][j1];
     }
@@ -78,7 +78,7 @@ void drugie_wb(std::vector<std::vector<double>>& v,double q_we)
      for(j=0;j<=ny;j++)
     {
         y=j*delta;
-        v[nx][j]=q_we/(2*ni)*(2*y-j1*delta-delta*ny);
+        v[nx][j]=q_we/(2*ni)*(2*y-delta*ny);
     }
     //brzeg B
     for(i=1;i<nx;i++)
@@ -92,16 +92,16 @@ void drugie_wb(std::vector<std::vector<double>>& v,double q_we)
         v[i][0]=2/(delta*delta)*(v[i][1]-v[i][0]);
     }
     //brzeg E
-    for(j=1;j<(j1-1);j++)
+    for(j=1;j<(j1);j++)
     {
         v[i1][j]=2/(delta*delta)*(v[i1+1][j]-v[i1][j]);
     }
     //brzeg F
-    for(i=1;i<i1;i++)
+    for(i=1;i<=i1;i++)
     {
         v[i][j1]=2/(delta*delta)*(v[i][j1+1]-v[i][j1]);
     }
-    v[i1-1][j1]=0.5*(v[i1-1][j1]+v[i1][j1-1]);
+    v[i1][j1]=0.5*(v[i1-1][j1]+v[i1][j1-1]);
 }
 
 int main()
